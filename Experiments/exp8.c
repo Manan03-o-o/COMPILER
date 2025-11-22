4a1ed41dd612f674229261937c56af922baafb8d
Manan <manannagpal003@gmail.com>
2025-11-22 22:40:03 +0530

    comments

--- Experiments/exp8.c
+++ Experiments/exp8.c
@@ -2,3 +2,25 @@
 #include <stdio.h> 
 int single_comment = 0, multi_comment = 0; 
 FILE *out;
+%} 
+ 
+%% 
+//.*             { single_comment++; } 
+"/\*"([^*]|\*+[^*/])*"*\*/"    { multi_comment++; } 
+.| 
+            { fputc(yytext[0], out); } 
+%% 
+ 
+int main() { 
+    out = fopen("no_comments.c", "w"); 
+    if (!out) { 
+        printf("Failed to open output file.\n"); 
+        return 1; 
+    } 
+    yylex(); 
+    fclose(out); 
+    printf("Single-line comments: %d\n", single_comment); 
+    printf("Multi-line comments: %d\n", multi_comment); 
+    printf("Cleaned file written to no_comments.c\n"); 
+    return 0; 
+}
