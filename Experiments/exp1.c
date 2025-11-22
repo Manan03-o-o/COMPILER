b4886511c7c805a6060da2f21e8fed7b1755caef
Manan <manannagpal003@gmail.com>
2025-11-22 22:36:02 +0530

    removed comments

--- Experiments/exp1.c
+++ Experiments/exp1.c
@@ -27,3 +27,33 @@ bool isKeyword(const char *str)
 void removeCommentsAndAnalyze(FILE *fp)  
  { 
      char ch, buffer[100]; 
+     int i = 0; 
+ 
+     while ((ch = fgetc(fp)) != EOF)  
+  { 
+         if (isspace(ch)) 
+            continue; 
+         if (ch == '/')  
+   { 
+             char next = fgetc(fp); 
+             if (next == '/')  
+    { 
+                 while ((ch = fgetc(fp)) != '\n' && ch != EOF); 
+                 continue; 
+    } 
+     else if (next == '*')  
+    { 
+                 while (1)  
+      { 
+                   ch = fgetc(fp); 
+                      if (ch == '*')  
+        {    
+  
+                           if ((ch = fgetc(fp)) == '/') 
+                              break; 
+        } 
+                      if (ch == EOF) break; 
+      } 
+                 continue; 
+    } 
+   else 
